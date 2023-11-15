af7e59ac64ca417a97ac4d8bb287814edbad09f5
sergei-zhirnov <s.zhirnov@protonmail.com>
2023-11-15 21:47:24 +0400

    M4.2 Final Task, Try 1

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int digit = 1;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (j == cathetusLength ||j == cathetusLength - i + 1 || (j < cathetusLength && j > cathetusLength - i + 1) ) {
+                    System.out.print(cathetusLength - j + 1);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int k = 1; k < i; k++) {
+                System.out.print(k + 1);
+            }
+            System.out.println();
+        }
 
     }
 
